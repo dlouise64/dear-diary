6e43269204914541fc7f8811b206f77b8c863266
dlouise64 <dana@hashbang.co.nz>
2014-08-02 18:14:35 +1200

    Add first and last name to user table, created author method for users first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+
+  def author
+    "#{first_name} #{last_name}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802055534) do
+ActiveRecord::Schema.define(version: 20140802060740) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20140802055534) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
