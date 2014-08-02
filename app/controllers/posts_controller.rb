148f41c25f36b0b2c1b52896bd6c2b08d14c7d6f
dlouise64 <dana@hashbang.co.nz>
2014-08-02 19:41:35 +1200

    Add edit and update action to post controller, also add post_params as required in rails 4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
   end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    if @post.update_attributes(post_params)
+      redirect_to action: 'show'
+    else
+      redirect_to action: 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
