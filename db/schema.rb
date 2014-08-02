783e877e053e406c4ca8e1dd5f808dd1ee73c82c
dlouise64 <dana@hashbang.co.nz>
2014-08-02 17:54:09 +1200

    Create posts model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802054955) do
+ActiveRecord::Schema.define(version: 20140802055321) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
