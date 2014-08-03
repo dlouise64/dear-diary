43420d7d82cd16ccf8d7519fdf265cf68ca4953a
Dana Iti <dana@hashbang.co.nz>
2014-08-04 00:00:57 +1200

    Add default scope to change sort order of posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
 
+  default_scope { order('created_at DESC') }
+  
   def post_date
     created_at.strftime('%d %b %Y')
   end
