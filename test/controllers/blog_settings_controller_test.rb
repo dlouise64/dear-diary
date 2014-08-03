83342b0936353407b520911e80bdd20cfb43191d
Dana Iti <dana@hashbang.co.nz>
2014-08-04 01:44:05 +1200

    Test files for blog settings

---
+++ test/controllers/blog_settings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BlogSettingsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/blog_settings_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BlogSettingsHelperTest < ActionView::TestCase
+end
